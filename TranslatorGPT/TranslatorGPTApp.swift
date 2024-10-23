748e758bfcdd52e59fcc07384c7fec33130a96dd
Vladislav Kovalskii <kovalskyvlad2412@icloud.com>
2024-10-23 11:06:01 +0400

    Initial Commit

---
+++ TranslatorGPT/TranslatorGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorGPTApp.swift
+//  TranslatorGPT
+//
+//  Created by Vladislav Kovalskii on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
